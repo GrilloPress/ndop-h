95cc93a32831c1d17511f4d92953a595178db1d8
grillopress <grillopress@gmail.com>
2018-01-08 14:56:52 +0000

    add calder routes

--- routes/router.js
+++ routes/router.js
@@ -92,4 +92,32 @@ router.get('/spen/:subdir/:subdir2/:view', function(req, res, next) {
 });
 // // // // // // //
 
+
+// GET v1 index page.
+router.get('/calder', function(req, res, next) {
+  res.render( 'calder/index' );
+});
+
+// GET all v1 URL reqs and push them to a template in the v1 file
+// This feels really brittle and hacky...
+// No handling of no view found...
+router.get('/calder/:view', function(req, res, next) {
+  var theView = req.params.view;
+  res.render( 'calder/' + theView );
+});
+
+router.get('/calder/:subdir/:view', function(req, res, next) {
+  var theView = req.params.view;
+  var theDir = req.params.subdir;
+  res.render( 'calder/' + theDir + '/' + theView );
+});
+
+router.get('/calder/:subdir/:subdir2/:view', function(req, res, next) {
+  var theView = req.params.view;
+  var theDir = req.params.subdir;
+  var theDir2 = req.params.subdir2;
+  res.render( 'calder/' + theDir + '/' + theDir2 + '/' + theView );
+});
+// // // // // // //
+
 module.exports = router;
